e9fa8aa320cc8ad403ce9f86ae79c57d4bae2bf2
Rajat Rajput <rajputrajat@gmail.com>
2021-03-08 19:02:11 +0530

    day 10

---
+++ day_10/if_else_example.py
@@ -0,0 +1,9 @@
+days = int(input("how many days are there in this month: "))
+if days == 31:
+    print("jan, mar, may, jul, aug, oct, dec")
+elif days == 28 or days == 29:
+    print("feb")
+elif days == 30:
+    print("apr, jun, sep, nov")
+else:
+    print("invalid input")
