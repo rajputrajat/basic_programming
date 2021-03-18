73f8a03d6f7ce1b6c0dcf999f7bff4ad79e04609
Rajat Rajput <rajputrajat@gmail.com>
2021-03-18 20:10:21 +0530

    while loop excercise

---
+++ day_12/repeat_chars_in_one_line.py
@@ -0,0 +1,12 @@
+var = 10
+while var != 20:
+    var = var + 3
+
+# char = input('which character: ')
+# times = int(input('how many times: '))
+
+# line = ''
+# while :
+#     line = line + char
+
+# print(line)
---
+++ day_13/while_loops_example.py
@@ -0,0 +1,77 @@
+var = 10
+counter = 0
+adjuster = 0
+while var > 0:
+    if counter % 2 == 0:
+        adjuster += 1
+    counter += 1
+    var -= 1
+print(adjuster)
+
+# var = 10
+# counter = 0
+# while var > 0:
+#     counter += 2
+#     var -= 1
+# print(counter)
+
+# var1 = 10
+# var2 = 20
+# while var1 < 20 or var2 > 10:
+#     var1 += 1
+#     var2 += 1
+
+# var1 = 10
+# var2 = 20
+# while var1 < 20 or var2 > 10:
+#     var1 -= 1
+#     var2 += 1
+
+# counter = 10
+# while counter >= 0:
+#     counter -= 1
+
+# var = '25'
+# var1 = '10'
+# while var1 < var:
+#     var1 += '1'
+
+# var4 = 10
+# while var4 * 2 <= 20:
+#     var4 += 1
+
+# var1 = 'aaa'
+# var2 = 15
+# while var2 > 10 and var1 < 'aaaaa':
+#     var2 = var2 + 1
+#     var1 = var1 + 'a'
+
+# counter = 0
+# while counter <= 100:
+#     counter = counter + 3
+
+# var1 = 15
+# var3 = 10
+# while var1 >= var3:
+#     var3 = var3 + 2
+
+# var1 = 15
+# var3 = 10
+# while var1 < var3:
+#     var3 = var3 + 2
+
+# var = 'a'
+# while 'aaa' > var:
+#     var = var + 'a'
+
+# var = 10
+# while 10 <= var:
+#     var = var - 5
+
+# var = 10
+# while var > 10:
+#     var = var + 5
+
+# var = 10
+# while var < 10:
+#     var = var + 1
