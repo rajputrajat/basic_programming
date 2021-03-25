04b3e14410ca552e616c9d38737983ae8ce6c9d3
Rajat Rajput <rajputrajat@gmail.com>
2021-03-25 20:41:20 +0530

    simplify

--- day_18/for_range.py
+++ day_18/for_range.py
@@ -2,7 +2,11 @@ char = input('which character: ')
 times = int(input('how many times: '))
 
 for a in range(times):
-    print((char + ' ') * (a + 1))
+    to_print = char + ' '
+    how_many_times = a + 1
+    make_a_line = to_print * how_many_times
+    print(make_a_line)
+    # print((char + ' ') * (a + 1))
 
 # range(4) == [0, 1, 2, 3]
 # range(10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
