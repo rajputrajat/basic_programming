83c04efa07dbabf1e08ba7d87f6c07960b11e475
Rajat Rajput <rajputrajat@gmail.com>
2021-03-27 19:31:53 +0530

    extend the hint

--- day_19/fruit_price_calculation.py
+++ day_19/fruit_price_calculation.py
@@ -1,2 +1,10 @@
 # fruit_name - weight in kgs - cost
 fruits_quantity = [['apples', 1, 100], ['oranges', 1.5, 60], ['grapes', 0.5, 120]]
+
+for fruit in fruits_quantity:
+    print(fruit, 'name:', fruit[0], 'weight:', fruit[1], 'cost:', fruit[2])
+    weight = fruit[1]
+    rate = fruit[2]
+    cost = weight * rate
+    print(cost)
+
