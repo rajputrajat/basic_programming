ef6dc38b8a7021cdcad75a1318d23bab56701cba
Rajat Rajput <rajputrajat@gmail.com>
2021-03-27 18:47:29 +0530

    for loop exercise

---
+++ day_19/exercises.py
@@ -0,0 +1,4 @@
+kids = ['Ira', 'Manya', 'Eshail', 'Abani']
+
+for kid in kids:
+    print(kid + ' is in class 2 now!')
---
+++ day_19/fruit_price_calculation.py
@@ -0,0 +1,2 @@
+# fruit_name - weight in kgs - cost
+fruits_quantity = [['apples', 1, 100], ['oranges', 1.5, 60], ['grapes', 0.5, 120]]
