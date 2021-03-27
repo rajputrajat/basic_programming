43802f006ee5ad83251a38fd29fa178e165c5274
Rajat Rajput <rajputrajat@gmail.com>
2021-03-27 19:55:55 +0530

    done

--- day_19/fruit_price_calculation.py
+++ day_19/fruit_price_calculation.py
@@ -1,10 +1,12 @@
 # fruit_name - weight in kgs - cost
 fruits_quantity = [['apples', 1, 100], ['oranges', 1.5, 60], ['grapes', 0.5, 120]]
 
+total_cost = 0.0
 for fruit in fruits_quantity:
-    print(fruit, 'name:', fruit[0], 'weight:', fruit[1], 'cost:', fruit[2])
+    #print(fruit, 'name:', fruit[0], 'weight:', fruit[1], 'cost:', fruit[2])
     weight = fruit[1]
     rate = fruit[2]
     cost = weight * rate
-    print(cost)
+    total_cost += cost
 
+print('total cost:', total_cost)
