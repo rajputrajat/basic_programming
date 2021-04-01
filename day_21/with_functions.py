6d2418081b0aa1b0e8794ddc826a48af8220cabc
Rajat Rajput <rajputrajat@gmail.com>
2021-04-01 20:06:58 +0530

    introduce functions

---
+++ day_21/with_functions.py
@@ -0,0 +1,15 @@
+def good_print(a_var):
+    print('value of this variable is:', a_var)
+    print('value of this variable is:', a_var)
+    print('value of this variable is:', a_var)
+    print('value of this variable is:', a_var)
+    print()
+
+a = 1
+good_print(a)
+
+b = 'hello'
+good_print(b)
+
+c = ['come', 'over', 'here']
+good_print(c)
---
+++ day_21/without_functions.py
@@ -0,0 +1,20 @@
+a = 1
+print('value of this variable is:', a)
+print('value of this variable is:', a)
+print('value of this variable is:', a)
+print('value of this variable is:', a)
+print()
+
+b = 'hello'
+print('value of this variable is:', b)
+print('value of this variable is:', b)
+print('value of this variable is:', b)
+print('value of this variable is:', b)
+print()
+
+c = ['come', 'over', 'here']
+print('value of this variable is:', c)
+print('value of this variable is:', c)
+print('value of this variable is:', b)
+print('value of this variable is:', c)
+print()
