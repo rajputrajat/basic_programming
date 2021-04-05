b1bc82b3d82b2832413b6c0e267bd2afaf8b84b1
Rajat Rajput <rajputrajat@gmail.com>
2021-04-05 19:11:16 +0530

    value dictionary

---
+++ day_24/value_dictionary.py
@@ -0,0 +1,18 @@
+# DICTIONARY data structure
+# key - value pairs
+# also called MAP in opther programming languages
+# also called just - dict => type
+
+addresses = {
+    'akash': 'afajsfallsjfaljfakfjalkf',
+    'sumit': 'sa laskflkasjfalksjfsa faslf lkdsfjaslkfja',
+    'ankit': 'adsajslgajslkajdlkfaj',
+    'amit': 'agsaglasjgaslkj'
+}
+
+address_of_amit = addresses['amit']
+print(address_of_amit)
+
+simple_ex = {1: 100, 2: 1000}
+a = simple_ex[1]
+print(a)
