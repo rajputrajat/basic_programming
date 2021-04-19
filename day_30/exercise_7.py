9000983f4d64f36940b59b74199016cd2fa4c2a0
Rajat Rajput <rajputrajat@gmail.com>
2021-04-19 19:02:30 +0530

    adding hint for exercise 7

---
+++ day_30/exercise_7.py
@@ -0,0 +1,11 @@
+# Exercise 9: Reverse a given number and
+# return true if it is the same as the original number
+# e.g. if number is 121 => return True
+#         number is 120 => return False
+# assert(is_magic_number(121) == True)
+
+a = 'true'
+a_l = list(a)
+print(a_l)
+a_l.reverse()
+print(a_l)
