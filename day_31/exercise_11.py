2479cb9aa691a2677c372b13c6150a472d587fa8
Rajat Rajput <rajputrajat@gmail.com>
2021-04-21 19:51:43 +0530

    exercise 11 boilerplate code

---
+++ day_31/exercise_11.py
@@ -0,0 +1,15 @@
+# Exercise 11: Write a code to extract each digit from an integer,
+# in the reverse order
+
+# Expected Output:
+# If the given int is 7536, the output shall be “6 3 5 7“,
+# with a space separating the digits.
+
+def extract_digit_in_reverse_order(num: int) -> str:
+    pass
+
+assert(extract_digit_in_reverse_order(7536) == '6 3 5 7')
+assert(extract_digit_in_reverse_order(1201) == '1 0 2 1')
+assert(extract_digit_in_reverse_order(82) == '2 8')
+
+print('passed')
